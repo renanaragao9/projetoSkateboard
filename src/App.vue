2283b616590d38e7c4df0f163f6b7455b63a0809
renanaragao9 <renanaragao159@gmail.com>
2024-11-05 23:01:53 -0300

    Subindo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <Dashboard/>
+</template>
+
+<script setup lang="ts">
+  import Dashboard from './layouts/Dashboard.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-gray-800 flex text-white max-h-screen h-screen">
+        <div class="w-64">
+            <span class="font-semibold p-6 pt-3 block opacity-90">skateboard</span>
+
+            <nav class="p-6">
+                <h3 class="opacity-50 uppercase font-semibold tracking-wider mb-6 text-xs">menu</h3>
+
+                <div class="space-y-6">
+                    <MenuItem :icon="HomeIcon">Discover </MenuItem>
+                    <MenuItem :icon="TrendingUpIcon">Trending </MenuItem>
+                </div>
+            </nav>
+        </div>
+
+        <div class="border-2 flex-1 border-red-500">Content</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import MenuItem from './navs/MenuItem.vue'
+    import HomeIcon from '../components/icons/HomeIcon.vue';
+    import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
+</script>
